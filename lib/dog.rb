7213e39b790c447398d3b49994336a5bdf37b8a5
HannaStrezh <hannastrezh@gmail.com>
2018-11-02 11:14:35 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,32 @@
 class Dog
+
 end
+
 fido = Dog.new
 snoopy = Dog.new
-lassie =Dog.new
+lassie = Dog.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+# end
+# fido = Dog.new
+# snoopy = Dog.new
+# lassie =Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,53 @@
 class Person
+
 end
- adele_goldberg = Person.new
- alan_kay = Person.new
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person
+# end
+#  adele_goldberg = Person.new
+#  alan_kay = Person.new
 
 
 
